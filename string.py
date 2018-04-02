3e5a64ccb58039d2f2edfa1b87bde1ea75746a53
sunjiaxing1992 <sunjiaxing1992@gmail.com>
2018-04-02 21:29:04 +0800

    string

---
+++ string.py
@@ -0,0 +1,35 @@
+name = "sUn JiAxing"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+## Sun Jiaxing
+## SUN JIAXING
+## sun jiaxing
+
+first_name = "sUn"
+last_name = "JiaXing"
+full_name = first_name + " " + last_name
+print('hello, ' + full_name.title())
+
+## hello, Sun Jiaxing
+
+print("Sun\nJiaxing")
+print("Sun\tJiaxing")
+print("Sun\n\tJiaxing")
+
+# Sun
+# Jiaxing
+# Sun     Jiaxing
+# Sun
+#         Jiaxing
+
+
+name = " sunjiaxing "
+print(name.strip())
+print(name.rstrip())
+print(name.lstrip())
+
+# sunjiaxing
+#  sunjiaxing
+# sunjiaxing
